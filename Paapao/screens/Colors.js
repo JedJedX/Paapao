44445701029cb8033b3c4a7bf8b7f5d561e6c9c9
Jedsada <s65122250025@ssru.ac.th>
2025-01-31 16:27:40 +0700

    add  card in colors

--- Paapao/screens/Colors.js
+++ Paapao/screens/Colors.js
@@ -1,11 +1,74 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView,View, Text } from 'react-native';
 import styles from "../styles/styles";
 
 export default function Colors() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Colors</Text>
-        </View>
+        <ScrollView contentContainerStyle={styles.container} keyboardShouldPersistTaps="handled">
+            <View style={styles.contentcard}>
+                <View style={styles.imgcard}>
+                    <Text style={styles.title}>img</Text>
+                </View>
+                <View style={styles.textcard}>
+                    <Text style={styles.title}>Colors</Text>
+                </View>
+            </View>
+            <View style={styles.contentcard}>
+                <View style={styles.imgcard}>
+                    <Text style={styles.title}>img</Text>
+                </View>
+                <View style={styles.textcard}>
+                    <Text style={styles.title}>Colors</Text>
+                </View>
+            </View>
+            <View style={styles.contentcard}>
+                <View style={styles.imgcard}>
+                    <Text style={styles.title}>img</Text>
+                </View>
+                <View style={styles.textcard}>
+                    <Text style={styles.title}>Colors</Text>
+                </View>
+            </View>
+            <View style={styles.contentcard}>
+                <View style={styles.imgcard}>
+                    <Text style={styles.title}>img</Text>
+                </View>
+                <View style={styles.textcard}>
+                    <Text style={styles.title}>Colors</Text>
+                </View>
+            </View>
+            <View style={styles.contentcard}>
+                <View style={styles.imgcard}>
+                    <Text style={styles.title}>img</Text>
+                </View>
+                <View style={styles.textcard}>
+                    <Text style={styles.title}>Colors</Text>
+                </View>
+            </View>
+            <View style={styles.contentcard}>
+                <View style={styles.imgcard}>
+                    <Text style={styles.title}>img</Text>
+                </View>
+                <View style={styles.textcard}>
+                    <Text style={styles.title}>Colors</Text>
+                </View>
+            </View>
+            <View style={styles.contentcard}>
+                <View style={styles.imgcard}>
+                    <Text style={styles.title}>img</Text>
+                </View>
+                <View style={styles.textcard}>
+                    <Text style={styles.title}>Colors</Text>
+                </View>
+            </View>
+            <View style={styles.contentcard}>
+                <View style={styles.imgcard}>
+                    <Text style={styles.title}>img</Text>
+                </View>
+                <View style={styles.textcard}>
+                    <Text style={styles.title}>Colors</Text>
+                </View>
+            </View>
+        </ScrollView>
     );
 };
--- Paapao/styles/styles.js
+++ Paapao/styles/styles.js
@@ -2,9 +2,31 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flexGrow: 1, 
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    contentcard: {
+        width: 200,
+        height: 200,
+        backgroundColor: "#ffb6c1",
+        marginBottom: 10,
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    imgcard: {
+        width: 80,
+        height: 80,
+        backgroundColor: "#add8e6",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    textcard: {
+        width: 80,
+        height: 80,
+        backgroundColor: "#add8e6",
+        justifyContent: "center",
+        alignItems: "center",
     },
     title: {
         fontSize: 24,
